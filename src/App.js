7a48f3df5f38bddb8331ae2b916218503fd66485
Jahongir Akramov <jahongirakramov0000@gmail.com>
2024-11-26 19:43:58 +0500

    test-onclose

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import './App.css';
 const tg = window.Telegram.WebApp;
 
 function App() {
+
+  const onClose = () => {
+    tg.close();
+  }
+
   return (
     <div className="App">
-      <button>
+      <button onClick={onClose}>
         close
       </button>
       <h1>
