be78f1a472aa5d2070e343aed0c733ac5d97cd51
Jahongir Akramov <jahongirakramov0000@gmail.com>
2024-11-27 16:13:15 +0500

    nuqta

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import "./App.css";
 const tg = window.Telegram.WebApp;
+const user = tg.initDataUnsafe?.user;
 
 function App() {
   useEffect(() => {
@@ -15,7 +16,7 @@ function App() {
     <div className="App">
       <button onClick={onClose}>close</button>
       <h1>salom</h1>
-      {(tg.initDataUnsafe?.user) ? <h2>{tg.initDataUnsafe?.user?.photo_url}</h2> : <h3>Hayr</h3>}
+      {(user) ? <h2>{...user}</h2> : <h3>Hayr</h3>}
     </div>
   );
 }
