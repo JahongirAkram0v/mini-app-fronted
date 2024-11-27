e2091ddd0fed0b4f4d50ac9b8ff491164d4dce1a
Jahongir Akramov <jahongirakramov0000@gmail.com>
2024-11-27 17:00:13 +0500

    git

--- src/App.js
+++ src/App.js
@@ -15,9 +15,25 @@ function App() {
   return (
     <div className="App">
       <button onClick={onClose}>close</button>
-      <h1>salom</h1>
-      <h2>{tg.initData}</h2>
-      {(user) ? <h2>{user.added_to_attachment_menu}</h2> : <h3>Hayr</h3>}
+      <h6>{tg.initDataUnsafe?.auth_date}</h6>
+      <br/>
+      <h6>{tg.initDataUnsafe?.can_send_after}</h6>
+      <br/>
+      <h6>{tg.initDataUnsafe?.chat}</h6>
+      <br/>
+      <h6>{tg.initDataUnsafe?.chat_instance}</h6>
+      <br/>
+      <h6>{tg.initDataUnsafe?.chat_type}</h6>
+      <br/>
+      <h6>{tg.initDataUnsafe?.hash}</h6>
+      <br/>
+      <h6>{tg.initDataUnsafe?.query_id}</h6>
+      <br/>
+      <h6>{tg.initDataUnsafe?.receiver}</h6>
+      <br/>
+      <h6>{tg.initDataUnsafe?.start_param}</h6>
+      <br/>
+      <h6>{user}</h6>
     </div>
   );
 }
