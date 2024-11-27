b38c1df1cd029f75ab2b06ef848ce2dbda6cddd8
Jahongir Akramov <jahongirakramov0000@gmail.com>
2024-11-27 16:40:46 +0500

    test6

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
     <div className="App">
       <button onClick={onClose}>close</button>
       <h1>salom</h1>
+      <h2>{JSON.parse(tg.initData)}</h2>
       {(user) ? <h2>{user.added_to_attachment_menu}</h2> : <h3>Hayr</h3>}
     </div>
   );
