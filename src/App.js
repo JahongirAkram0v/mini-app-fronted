be4886d8cb37f45d40583d695dc4e123c75dfe0d
Jahongir Akramov <jahongirakramov0000@gmail.com>
2024-11-27 17:22:59 +0500

    git

--- src/App.js
+++ src/App.js
@@ -15,8 +15,6 @@ function App() {
   return (
     <div className="App">
       <button onClick={onClose}>close</button>
-      <h6>{tg.initDataUnsafe?.chat_type}</h6>
-      <h6>{tg.initDataUnsafe?.query_id}</h6>
       <h6>{tg.initDataUnsafe?.receiver?.first_name}</h6>
       <h6>{user?.first_name}</h6>
     </div>
